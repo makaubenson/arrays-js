ceba925075d98e7fe4a406baa91b30a6cdefb43b
Benson <bensonmakau2000@gmail.com>
2022-06-07 21:32:02 +0300

    calculated interest

--- script.js
+++ script.js
@@ -90,7 +90,10 @@ labelSumIn.textContent = `${incomes}€`;
 
 const out = movements.filter(mov => mov < 0).reduce((acc,mov) => acc + mov, 0);
 labelSumOut.textContent = `${Math.abs(out)}€`;
-}
+
+const interest = movements.filter(mov => mov > 0).map(deposit => deposit * 1.2/100 ).reduce((acc,int) => acc + int, 0)
+labelSumInterest.textContent = `${interest}`;
+};
 calcDisplaySummary(account1.movements);
 
 
