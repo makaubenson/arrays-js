31ebf21215676f2222710ad8080ac5a443500679
Benson <bensonmakau2000@gmail.com>
2022-06-07 22:09:45 +0300

    chaining methods in practice

--- script.js
+++ script.js
@@ -115,6 +115,12 @@ const createUsernames = function (accs) {
   // console.log(accounts);
 };
 createUsernames(accounts);
+
+//Chaining Methods - PIPELINE
+const eurToUsd = 1.1;
+const totalDepositsUSD = movements.filter(mov => mov > 0).map(mov => mov * eurToUsd).reduce((acc,mov) => acc + mov,0);
+// console.log(totalDepositsUSD);
+
 /*
 //filter()
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -394,8 +400,34 @@ const movementsDescriptions = movements.map((mov, i, arr) => {
 });
 console.log(movementsDescriptions);
 */
+///////////////////////////////////////
+// Coding Challenge #3
 
-//Chaining Methods - PIPELINE
-const eurToUsd = 1.1;
-const totalDepositsUSD = movements.filter(mov => mov > 0).map(mov => mov * eurToUsd).reduce((acc,mov) => acc + mov,0);
-// console.log(totalDepositsUSD);
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+const calcAverageHumanAge1 = function (ages) {
+  const humanAges = ages.map(age => age <= 2 ? 2 * age : 16 + age *4);
+// console.log(humanAges);
+//filter dogs atleast 18 years old
+const adults= humanAges.filter(age => age >= 18);
+// console.log(humanAges);
+// console.log(adults);
+
+//  const average = adults.reduce((acc,age) => acc + age,0) / adults.length;
+ const average = adults.reduce((acc,age,i,arr) => acc + age / arr.length,0);
+return average;
+};
+
+
+
+const calcAverageHumanAge = ages => ages.map(age => age <= 2 ? 2 * age : 16 + age *4).filter(age => age >= 18).reduce((acc,age,i,arr) => acc + age / arr.length,0);
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1,avg2);
