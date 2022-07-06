36f3f9363dc9f1d33d4b4996c22aa9fdbc9844b2
Benson <bensonmakau2000@gmail.com>
2022-07-06 15:18:30 +0300

    loop over the dogs array

--- script.js
+++ script.js
@@ -779,3 +779,13 @@ const dogs = [
 
 GOOD LUCK 😀
 */
+// 1.
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+console.log(dogs);
