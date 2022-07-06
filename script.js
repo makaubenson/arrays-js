fcf37881b6cdb61f97cd7d041c00582b5a4358f8
Benson <bensonmakau2000@gmail.com>
2022-07-06 09:25:07 +0300

    replaced regular functions with arrow functions

--- script.js
+++ script.js
@@ -610,20 +610,34 @@ const overalBalance1 = accounts
   }, 0);
 console.log(overalBalance1);
 
+*/
 
 //sorting arrays
 //Strings
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
 console.log(owners.sort()); //['Adam', 'Jonas', 'Martha', 'Zach']
 //numbers
-console.log(movements); // [200, 450, -400, 3000, -650, -130, 70, 1300]
-console.log(movements.sort()); //[-130, -400, -650, 1300, 200, 3000, 450, 70]
+// console.log(movements); // [200, 450, -400, 3000, -650, -130, 70, 1300]
+// console.log(movements.sort()); //[-130, -400, -650, 1300, 200, 3000, 450, 70]
 //to handle the error in sorting numbers, we pass the compare callback function in sort method
 //return < 0, A,B (KEEP ORDER)
 //return > 0, B,A (SWITCH ORDER)
-movements.sort(function (a, b) {
-  if (a > b) return 1;
-  if (b > a) return -1;
-});
+
+//ascending
+// movements.sort(function (a, b) {
+//   if (a > b) return 1;
+//   if (b < a) return -1;
+// });
+//arrow function
+movements.sort((a, b) => a - b);
 console.log(movements); // [-650, -400, -130, 70, 200, 450, 1300, 3000]
-*/
+
+//descending
+// movements.sort(function (a, b) {
+//   if (a > b) return -1;
+//   if (b < a) return 1;
+// });
+
+//arrow function
+movements.sort((a, b) => b - a);
+console.log(movements); //[3000, 1300, 450, 200, 70, -130, -400, -650]
