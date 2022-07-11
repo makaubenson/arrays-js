99751882fab9aa3749fc19be1b1a1695b38c9980
Benson <bensonmakau2000@gmail.com>
2022-07-11 23:09:52 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 'use strict';
-
-/////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -229,7 +227,6 @@ btnSort.addEventListener('click', function (e) {
 // const eurToUsd = 1.1;
 // const totalDepositsUSD = movements.filter(mov => mov > 0).map(mov => mov * eurToUsd).reduce((acc,mov) => acc + mov,0);
 // console.log(totalDepositsUSD);
-
 
 //find() method
 // const firstWithdrawal = movements.find(mov => mov < 0);
